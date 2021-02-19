94891863d2fd427c8e2190f08433b33609e8519b
Vaibhav Gupta <47090589+Aatmsaat@users.noreply.github.com>
2021-02-19 18:51:42 +0530

    Packages for Person in Jungle
    Dot net c# game with the help of windows form based on a person travelling in jungle and captured by carnivorous plants

---
+++ PersonInJungle/PersonInJungle/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace PersonInJungle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.person = new System.Windows.Forms.PictureBox();
+            this.obstacle3 = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.person)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.scoreText.Location = new System.Drawing.Point(12, 9);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(100, 25);
+            this.scoreText.TabIndex = 7;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.ForestGreen;
+            this.floor.Location = new System.Drawing.Point(-12, 452);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(652, 50);
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            // 
+            // person
+            // 
+            this.person.Image = global::PersonInJungle.Properties.Resources.person2;
+            this.person.Location = new System.Drawing.Point(79, 369);
+            this.person.Name = "person";
+            this.person.Size = new System.Drawing.Size(57, 85);
+            this.person.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.person.TabIndex = 0;
+            this.person.TabStop = false;
+            // 
+            // obstacle3
+            // 
+            this.obstacle3.Image = global::PersonInJungle.Properties.Resources.plnt3;
+            this.obstacle3.Location = new System.Drawing.Point(529, 394);
+            this.obstacle3.Name = "obstacle3";
+            this.obstacle3.Size = new System.Drawing.Size(57, 63);
+            this.obstacle3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle3.TabIndex = 6;
+            this.obstacle3.TabStop = false;
+            this.obstacle3.Tag = "obstacle";
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::PersonInJungle.Properties.Resources.plnt1;
+            this.obstacle1.Location = new System.Drawing.Point(344, 400);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(57, 63);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle1.TabIndex = 4;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::PersonInJungle.Properties.Resources.plnt2;
+            this.obstacle2.Location = new System.Drawing.Point(448, 394);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(57, 63);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 5;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(618, 475);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.obstacle3);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.person);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "The Person In the Jungle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.person)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox person;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox obstacle3;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ PersonInJungle/PersonInJungle/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PersonInJungle
+{
+    public partial class Form1 : Form
+    {
+        //private const var txtclr = scoreText.ForeColor;
+        bool jumping = false; //if player jumping or not
+        int jumpSpeed = 10; //to set jump speed
+        int force = 20; //force of the jump
+        int score = 0;
+        int obstacleSpeed = 10;//default speed of obstacle
+        Random rnd = new Random();// new random class created
+        static bool death = false;
+        
+
+     
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameEvent(object sender, EventArgs e)
+        {
+            person.Top += jumpSpeed;
+            scoreText.Text = "Score: " + score;
+            if (jumping && force<0)
+            {
+                jumping = false;
+            }
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+                    if (x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + rnd.Next(200, 800);
+                        try
+                        {
+                            SoundPlayer snd = new SoundPlayer(Properties.Resources.point);
+                            snd.Play();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error : " + ex.Message);
+                        }
+
+                        score++;
+                    }
+                    if (person.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        person.Image = Properties.Resources.per3;
+                        scoreText.Text += " Press R to restart";
+                        scoreText.ForeColor = Color.Red;
+                        death = true;
+                        try
+                        {
+                            SoundPlayer snd1 = new SoundPlayer(Properties.Resources.fearfemale);
+                            SoundPlayer snd2 = new SoundPlayer(Properties.Resources.game_over_words);
+                            snd1.Play();
+                            //add time gap to clearly listening both voices
+                            Thread.Sleep(1000);
+                            snd2.Play();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error : " + ex.Message);
+                        }
+                    }
+                }
+            }
+
+            if (person.Top >= 290 && !jumping)
+            {
+                force = 20;
+                person.Top = floor.Top - person.Height;
+                jumpSpeed = 0;
+            }
+
+            if (score >= 10)
+            {
+                obstacleSpeed = 15;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jumping && !death)
+            {
+                try
+                {
+                    SoundPlayer snd = new SoundPlayer(Properties.Resources.jump);
+                    snd.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error : " + ex.Message);
+                }
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        public void resetGame()
+        {
+            jumping = false; //if player jumping or not
+            jumpSpeed = 10; //to set jump speed
+            force = 20; //force of the jump
+            score = 0;
+            obstacleSpeed = 10;//default speed of obstacle
+            person.Top = floor.Top - person.Height;
+            scoreText.Text = "Score: " + score;
+            scoreText.ForeColor = Color.ForestGreen;
+            person.Image = Properties.Resources.person2;
+            death = false;
+            
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left = 640 + (x.Left + rnd.Next(600, 1000) + x.Width * 3);
+                }
+            }
+            gameTimer.Start();
+        }
+    }
+}
